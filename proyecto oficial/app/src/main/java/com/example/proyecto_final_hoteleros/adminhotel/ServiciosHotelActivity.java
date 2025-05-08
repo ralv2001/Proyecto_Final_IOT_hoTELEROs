573dc6fa3152f066bc9cce62ee7863044383fb27
JACORVI <a.coronadov@pucp.edu.pe>
2025-05-08 00:11:40 -0500

    actv1

---
+++ proyecto oficial/app/src/main/java/com/example/proyecto_final_hoteleros/adminhotel/ServiciosHotelActivity.java
@@ -0,0 +1,15 @@
+package com.example.proyecto_final_hoteleros.adminhotel;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.proyecto_final_hoteleros.R;
+
+public class ServiciosHotelActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.admin_hotel_servicios);
+    }
+}
