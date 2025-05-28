f18b57f756b85d63bb1d75db532b05c63f287692
JACORVI <a.coronadov@pucp.edu.pe>
2025-05-28 00:51:46 -0500

    commit

---
+++ proyecto oficial/app/src/main/java/com/example/proyecto_final_hoteleros/adapters/HuespedAdapter.java
@@ -0,0 +1,52 @@
+package com.example.proyecto_final_hoteleros.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.proyecto_final_hoteleros.R;
+import com.example.proyecto_final_hoteleros.adminhotel.Huesped;
+
+import java.util.List;
+
+public class HuespedAdapter extends RecyclerView.Adapter<HuespedAdapter.HuespedViewHolder> {
+
+    private List<Huesped> listaHuespedes;
+
+    public HuespedAdapter(List<Huesped> listaHuespedes) {
+        this.listaHuespedes = listaHuespedes;
+    }
+
+    @NonNull
+    @Override
+    public HuespedViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View vista = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_huesped, parent, false);
+        return new HuespedViewHolder(vista);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull HuespedViewHolder holder, int position) {
+        Huesped h = listaHuespedes.get(position);
+        holder.tvNombre.setText(h.getNombre());
+        holder.tvCheckIn.setText("Check-in: " + h.getCheckIn());
+    }
+
+    @Override
+    public int getItemCount() {
+        return listaHuespedes.size();
+    }
+
+    public static class HuespedViewHolder extends RecyclerView.ViewHolder {
+        TextView tvNombre, tvCheckIn;
+
+        public HuespedViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvNombre = itemView.findViewById(R.id.tvNombre);
+            tvCheckIn = itemView.findViewById(R.id.tvCheckIn);
+        }
+    }
+}
